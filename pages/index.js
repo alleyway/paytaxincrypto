52a6dd67214e5bb6a9942ed999118ff5238d6672
mlake <mlake@alleywayapps.com>
2021-01-02 12:50:32 +0100

    small spacing change

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+import React from "react"
+import Container from "@material-ui/core/Container"
+import Typography from "@material-ui/core/Typography"
+import Box from "@material-ui/core/Box"
+import ProTip from "../src/ProTip"
+import Link from "../src/Link"
+import Copyright from "../src/Copyright"
 import JurisdictionsTable from "../src/JurisdictionsTable"
 
-export default function Index() {
-  return (
-      <Container maxWidth="md" disableGutters>
-        <Box my={6}>
-          <Typography variant="h3" component="h1" align="center" gutterBottom>
-            Tax Jurisdictions Accepting Cryptocurrency
-          </Typography>
+export default function Index () {
+    return (
+        <Container maxWidth="md" disableGutters>
+            <Box my={6}>
+                <Typography variant="h3" component="h1" align="center" gutterBottom>
+                    Tax Jurisdictions Accepting Cryptocurrency
+                </Typography>
 
-            <JurisdictionsTable/>
+                <JurisdictionsTable/>
 
-          {/*<Link href="/about" color="secondary">*/}
-          {/*  Go to the about page*/}
-          {/*</Link>*/}
-          <Copyright />
-        </Box>
-      </Container>
-  );
+                {/*<Link href="/about" color="secondary">*/}
+                {/*  Go to the about page*/}
+                {/*</Link>*/}
+
+            </Box>
+            <Copyright/>
+        </Container>
+    )
 }
