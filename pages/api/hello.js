17353d21b1a2b84d31bcd2dced72c58e185766c3
mlake <mlake@alleywayapps.com>
2021-03-02 08:31:05 +0100

    updating server

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+
+  fetch({url:'https://api.binance.com/api/v3/exchangeInfo', method: "get"}).then((response)=>{
+
+    const message = response.statusText
+
+    res.json({ name: 'John Doe', message })
+
+  }).catch((e) => {
+
+    res.statusCode = 500
+    res.json({ name: 'error' })
+
+  })
+
+
 }
